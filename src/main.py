a2b37bcd6ffe2e2f0309a74e072ef86de1cfbd5b
Hemerson Carlos <hemersoncarlos93@hotmail.com>
2022-07-13 20:13:27 -0300

    Add comment for inicialization app.

--- src/main.py
+++ src/main.py
@@ -2,34 +2,34 @@ from movimentation_logic import Exploration
 from knowledge_base import KnowledgeBase
 from environment import WumpusWorld
 from environment import size
-#from view import ViewMatrix
+from view import ViewMatrix
 
 
 class Main:
 
-    print('\nGerando a configuração inicial do Mundo do Wumpus ' + str(size - 2) + 'x' + str(size - 2) + '...\n')
-    world = WumpusWorld()
+    #print('\nGerando a configuração inicial do Mundo do Wumpus ' + str(size - 2) + 'x' + str(size - 2) + '...\n')
+    #world = WumpusWorld()
 
-    print("\n\t MUNDO GERADO:")
-    print('\n')
-    for count in range(size):
-        print(world.field[count])
-    print('\n')
-    for count in range(size):
-        print(world.perceptions[count])
+    #print("\n\t MUNDO GERADO:")
+    #print('\n')
+    #for count in range(size):
+        #print(world.field[count])
+    #print('\n')
+    #for count in range(size):
+        #print(world.perceptions[count])
 
-    base = KnowledgeBase(world)
-    explore = Exploration(world, base)
-    explore.move_agent()
+    #base = KnowledgeBase(world)
+    #explore = Exploration(world, base)
+    #explore.move_agent()
 
-    if explore.gold:
-        print('\nOURO ENCONTRADO!')
-    else:
-        print('\nO agente NÃO CONSEGUIU encontrar o ouro!')
+    #if explore.gold:
+        #print('\nOURO ENCONTRADO!')
+    #else:
+        #print('\nO agente NÃO CONSEGUIU encontrar o ouro!')
 
-    print('\nPONTUAÇÃO: ' + str(explore.points) + '\n')
+    #print('\nPONTUAÇÃO: ' + str(explore.points) + '\n')
 
-    #view = ViewMatrix()
+    view = ViewMatrix()
 
 
 if __name__ == '__main__':
